77ac023aa6c3c2a235009cbbd3e8aaa53423904e
Mathis Barré <mathis.barre@live.fr>
2020-11-28 11:44:02 +0100

    Setup project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './**/*.html',
+    './**/*.js',
+    './**/*.jsx',
+    './**/*.tsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
